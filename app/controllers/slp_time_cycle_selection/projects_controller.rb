449b462e442c428e7a56fab9a0efa2d11dcc58c0
bill1997603 <fan1997603@vip.qq.com>
2019-08-08 10:07:06 +0800

    feat: project controller

---
+++ app/controllers/slp_time_cycle_selection/projects_controller.rb
@@ -0,0 +1,9 @@
+require_dependency "slp_time_cycle_selection/application_controller"
+
+module SlpTimeCycleSelection
+  class ProjectsController < ApplicationController
+    def index
+      
+    end
+  end
+end
