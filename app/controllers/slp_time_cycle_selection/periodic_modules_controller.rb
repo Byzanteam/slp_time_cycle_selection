967da833002d13b0fcaeabf68ac3f4099604c870
bill1997603 <fan1997603@vip.qq.com>
2019-08-08 15:19:39 +0800

    feat: periodic module index

--- app/controllers/slp_time_cycle_selection/periodic_modules_controller.rb
+++ app/controllers/slp_time_cycle_selection/periodic_modules_controller.rb
@@ -2,5 +2,17 @@ require_dependency "slp_time_cycle_selection/application_controller"
 
 module SlpTimeCycleSelection
   class PeriodicModulesController < ApplicationController
+    before_action :set_project
+
+    def index
+      @periodic_modules = paginate @project.periodic_modules
+      render layout: false
+    end
+
+    private
+
+    def set_project
+      @project = SlpTimeCycleSelection::Project.find(params[:project_id])
+    end
   end
 end
