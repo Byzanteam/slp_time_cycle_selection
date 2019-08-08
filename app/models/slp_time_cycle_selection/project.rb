bcb024aa0147b197fe00b7db77d7e0796da3f96a
bill1997603 <fan1997603@vip.qq.com>
2019-08-08 11:00:59 +0800

    add: project column present

--- app/models/slp_time_cycle_selection/project.rb
+++ app/models/slp_time_cycle_selection/project.rb
@@ -1,4 +1,12 @@
 module SlpTimeCycleSelection
   class Project < ApplicationRecord
+    validates :delay_minutes, presence: true
+    validates :delay_minute_unit, presence: true
+
+    enum delay_minute_unit: {
+      'minute' => '1',
+      'hour' => '2',
+      'day' => '3'
+    }
   end
 end
