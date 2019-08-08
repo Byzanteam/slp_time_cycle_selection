4c1326026c9f4754ee0cb58e7af4f64e210c6b85
bill1997603 <fan1997603@vip.qq.com>
2019-08-08 17:32:01 +0800

    add: enum

--- app/models/slp_time_cycle_selection/periodic_module_day.rb
+++ app/models/slp_time_cycle_selection/periodic_module_day.rb
@@ -1,4 +1,15 @@
 module SlpTimeCycleSelection
   class PeriodicModuleDay < ApplicationRecord
+    validates :name, presence: true
+
+    enum name: {
+      'Monday' => 0,
+      'Tuesday' => 1,
+      'Wednesday' => 2,
+      'Thursday' => 3,
+      'Friday' => 4,
+      'Saturday' => 5,
+      'Sunday' => 6
+    }
   end
 end
