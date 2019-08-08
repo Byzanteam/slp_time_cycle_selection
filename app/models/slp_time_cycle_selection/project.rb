c5fe7f0d01ed7c741d0624a02021aae1b4c8edf6
bill1997603 <fan1997603@vip.qq.com>
2019-08-08 15:06:03 +0800

    feat: periodic module

--- app/models/slp_time_cycle_selection/project.rb
+++ app/models/slp_time_cycle_selection/project.rb
@@ -1,9 +1,19 @@
 module SlpTimeCycleSelection
   class Project < ApplicationRecord
+    before_create :set_default_periodic_module
+
+    has_many :periodic_modules, :class_name => 'SlpTimeCycleSelection::PeriodicModule'
+
     enum delay_minute_unit: {
       'minute' => 0,
       'hour' => 1,
       'day' => 2
     }
+
+    private
+
+    def set_default_periodic_module
+      periodic_modules.create(name: '默认')
+    end
   end
 end
