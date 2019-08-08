a0cfe4a09e0f8b688ab1ce94e6ca0948d77d9b9c
bill1997603 <fan1997603@vip.qq.com>
2019-08-08 18:00:09 +0800

    feat: set_default_periodic_module_days

--- app/models/slp_time_cycle_selection/periodic_module.rb
+++ app/models/slp_time_cycle_selection/periodic_module.rb
@@ -2,5 +2,17 @@ module SlpTimeCycleSelection
   class PeriodicModule < ApplicationRecord
     belongs_to :project, :class_name => 'SlpTimeCycleSelection::Project'
     has_many :periodic_module_days, :class_name => 'SlpTimeCycleSelection::PeriodicModuleDay', dependent: :destroy
+
+    after_create :set_default_periodic_module_days
+
+    private
+
+    def set_default_periodic_module_days
+      ActiveRecord::Base.transaction do
+        (0..6).each do |day|
+          periodic_module_days.create(name: day)
+        end
+      end
+    end
   end
 end
