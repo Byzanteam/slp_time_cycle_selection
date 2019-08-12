0870db4f595221530aac19830ba1289ef03b8ee1
bill1997603 <fan1997603@vip.qq.com>
2019-08-12 14:31:57 +0800

    refactor: before action

--- app/models/slp_time_cycle_selection/periodic_module_option.rb
+++ app/models/slp_time_cycle_selection/periodic_module_option.rb
@@ -3,31 +3,15 @@ module SlpTimeCycleSelection
     belongs_to :module_date, class_name: 'SlpTimeCycleSelection::PeriodicModuleDate', foreign_key: 'periodic_module_date_id'
     delegate :periodic_module, to: :module_date
 
-    after_commit :set_option
-
-    private
-
-    def set_option
-      return unless start_at
-
-      option.clear
-      option << start_at.strftime('%T')
-      option << end_at.strftime('T') if start_at.present? && end_at.present?
-
-      temp = option.reduce([]) do |memo, str|
-        memo << "'" + str + "'"
+    def save(attributes)
+      if start_at.present?
+        option.clear
+        option << start_at.strftime('%T')
       end
 
-      sql = <<-SQL
-        update
-          slp_time_cycle_selection_periodic_module_options
-        set
-          option = ARRAY[#{temp.join(',')}]
-        where
-          id = #{id}
-      SQL
+      option << end_at.strftime('T') if end_at.present?
 
-      ActiveRecord::Base.connection.execute(sql)
+      super(attributes)
     end
   end
 end
