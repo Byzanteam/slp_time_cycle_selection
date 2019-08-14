0b9183e58526d213d900d50539ec75f3bd17fcd9
bill1997603 <fan1997603@vip.qq.com>
2019-08-14 10:30:00 +0800

    feat: update profile dates

--- app/controllers/slp_time_cycle_selection/profile_dates_controller.rb
+++ app/controllers/slp_time_cycle_selection/profile_dates_controller.rb
@@ -8,6 +8,21 @@ module SlpTimeCycleSelection
       @profile_date = @project.profile_date
     end
 
+    def update
+      workday = params[:workday]
+      holiday = params[:holiday]
+      profile_date = @project.profile_date
+
+      if workday
+        profile_date.set_workday(workday)
+      else
+        profile_date.set_holiday(holiday)
+      end
+      render partial: 'slp_time_cycle_selection/profile_dates/profile_date',
+             locals: { profile_date: profile_date },
+             layout: false
+    end
+
     private
 
     def set_project
